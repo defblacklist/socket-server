10fe8c675cc26384f6fdc0c3b516b4be65171484
defblacklist <l1611409064@stud.sdu.edu.tr>
2022-09-01 15:41:50 +0300

    first commit

---
+++ blacklist_socket.js
@@ -0,0 +1,24 @@
+﻿const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket){
+    console.log('sockete birileri bağlandı');
+
+    socket.on('yeni-mesaj', function(data){
+        io.emit('mesajlar', data);
+    });
+
+    socket.on('gelen-mesaj', function(data){
+        io.emit('yeni-gelen', data);
+    });
+
+    socket.on('giden-mesaj', function(data){
+        io.emit('yeni-giden', data);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('birileri geldi ve gitti');
+    });
+});
+
+server.listen(5000);
